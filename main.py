f798b1a604672c4bfff61700c7d6d0140229f641
dickylai <dickytwlai@gmail.com>
2018-01-21 00:25:31 +0800

    support saving links to file

--- main.py
+++ main.py
@@ -3,15 +3,28 @@
 from bs4 import BeautifulSoup
 import requests
 import time
+import sys
 
 # site for scraping
-domain = 'http://bongdanet.vn'
+DOMAIN = 'http://bongdanet.vn'
+# path for saving links
+LINKSPATH = sys.argv[1] if len(sys.argv) > 1 else None;
 
 def makeSoupFromUrl(domain, url):
 	page = requests.get(domain + url)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	page.close()
 	return soup
+
+def printLinks(links):
+	print ('Links are as follows: ')
+	print (links[0].get_text().strip())
+
+def saveLinksToFile(links, filePath):
+	print ('Saving links to ' + filePath + '...')
+	with open(filePath, 'w+') as linksFile:
+		linksFile.write(links[0].get_text().strip())
+	print ('Done.')
 
 def exitProgram():
 	print ()
@@ -32,7 +45,7 @@ try:
 		secondTeam = input('Please input the second team: (OPTIONAL) ')
 
 	# get all the matches
-	matches = makeSoupFromUrl(domain, '/link-sopcast').select('div.stream-info div.competeTeams div.livetv-thumbnail a') 
+	matches = makeSoupFromUrl(DOMAIN, '/link-sopcast').select('div.stream-info div.competeTeams div.livetv-thumbnail a')
 
 	# get the required match
 	matchUrl = ''
@@ -47,16 +60,17 @@ try:
 		exit()
 
 	# get all the sopcast links for the required match
-	sopcastLinks = makeSoupFromUrl(domain, matchUrl).select('ul.zone-sopcast-links')
+	sopcastLinks = makeSoupFromUrl(DOMAIN, matchUrl).select('ul.zone-sopcast-links')
 
 	while not sopcastLinks:
 		print('No link is available at this moment. Auto retry in a minute.')
 		time.sleep(60)
 		sopcastLinks = makeSoupFromUrl(domain, matchUrl).select('ul.zone-sopcast-links')
 
-	print ('Links: ')
-	for link in sopcastLinks:
-		print(link.get_text())
+	if LINKSPATH is None:
+		printLinks(sopcastLinks)
+	else:
+		saveLinksToFile(sopcastLinks, LINKSPATH)
 
 except KeyboardInterrupt:
 	exitProgram()
